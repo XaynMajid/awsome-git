17622b70f1d054a0647e95ffd160a3dd0ab3359c
Zain Majid <zainmajid0078@gmail.com>
2025-03-20 15:38:49 -1200

    message

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ console.log(a+b+c);
 let d= 40;
 
 console.log(b+d,'This is Sum ');
-var ab =10;
+var abc =10;
 console.log(ab+a+b+c+d)
 
 console.log(a+b+c+d+c+e)
 console.log('hi')
 
 console.log('hello form new repo')
+
+let a1 = 1;
+let b2 = 2;
+
+console.log(a1+b2)
